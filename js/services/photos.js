5c0c615a869be778030b8d389ac4c7614e86508c
RDP <rdupaw@gmail.com>
2017-03-08 20:01:16 -0600

    Fixed .get url from http:// to https://

--- js/services/photos.js
+++ js/services/photos.js
@@ -1,5 +1,5 @@
 app.factory('photos', ['$http', function($http) {
-  return $http.get('http://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
+  return $http.get('https://s3.amazonaws.com/codecademy-content/courses/ltp4/photos-api/photos.json')
          .success(function(data) {
            return data;
          })
